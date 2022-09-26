53349705f7bf0d38f7ca41193be9f6cd3d5399f4
Anton <pavlov@mail.ru>
2022-09-26 21:13:35 +0500

    Zadacha 23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,30 @@
+﻿int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void  PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+ string LineNum(int n, int P)
+{
+    string outline = "1";
+    for (int i=2; i<=n; i++ )
+{
+    outline = outline+ " | " + Math.Pow(i,P);
+
+}
+    return outline;
+}
+
+int n = ReadData("Введите целое число ");
+
+string linetop = LineNum(n,1);
+string linedown = LineNum(n,3);
+
+PrintData(" ", linetop);
+PrintData(" ", linedown);
